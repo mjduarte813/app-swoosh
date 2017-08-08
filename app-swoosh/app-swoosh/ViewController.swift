d2789f293bd6835c29f4941eb0b21e637d2ebc02
Marcel <marceljduarte@gmail.com>
2017-08-07 19:59:09 -0600

    lesson-03: Auto Layout

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -17,11 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Dynamic UI without Autolayout
-        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height) //X gives you the far left of the frame.
-        
-        //Get view frame and set to= background image frame. Fill the whole screen
-        bgImg.frame = view.frame
+       
     }
 
     override func didReceiveMemoryWarning() {
