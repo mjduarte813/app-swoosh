047ee5535c908a36cececf6211572d0aefb1cc4c
Marcel <marceljduarte@gmail.com>
2017-08-07 19:00:52 -0600

    Second lesson - Frames and Bounds

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Dynamic UI without Autolayout
+        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height) //X gives you the far left of the frame.
+        
+        //Get view frame and set to= background image frame. Fill the whole screen
+        bgImg.frame = view.frame
     }
 
     override func didReceiveMemoryWarning() {
